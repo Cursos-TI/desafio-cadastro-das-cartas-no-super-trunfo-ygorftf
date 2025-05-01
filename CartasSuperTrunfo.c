2dda2fd5ac4f6f1bcb74603f09188437c0c39a75
Ygor Torres <123262721+ygorftf@users.noreply.github.com>
2025-05-01 15:17:35 -0300

    Atualização do codigo aula 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    char estado, codigo[4], cidade[100], estado2, codigo2[4], cidade2[100];
+    /*Variaveis ambas as cartas */
+    char estado[3], codigo[4], cidade[100], estado2[3], codigo2[4], cidade2[100];
     int populacao, populacao2;
     float area, pib, area2, pib2;
     int numeroPontosTuristicos, numeroPontosTuristicos2;
+    float densidadeP = populacao / area;
+    float densidadeP2 = populacao2 / area2;
+    float pibPerCapita = pib / populacao;
+    float pibPerCapita2 = pib2 / populacao2;
 
-    printf("Olá !!\nVamos cadastrar a carta nº: 01\n");
+    printf("BEM-VINDO AO SUPER TRUNFO!\n\nVamos cadastrar a carta nº: 01\n"); /*Cadastro carta 1*/
     
     printf("Digite o estado: \n");
-    scanf(" %c", &estado);
+    scanf("%s", estado);
     
+    getchar();
     printf("Digite o codigo: \n");
     scanf("%s", codigo);
     
+    getchar();
     printf("Digite o nome da cidade: \n");
     scanf(" %[^\n]%*c", cidade);
 
@@ -30,13 +38,15 @@ int main(){
     printf("Digite o numero de pontos turisticos: \n");
     scanf("%d", &numeroPontosTuristicos);
 
-    printf("\n ------------------------------------------------- \n\n");
+    system("clear");
 
-    printf("Agora vamos cadastrar a carta nº: 02\n");
+    printf("Agora vamos cadastrar a carta nº: 02\n\n"); /*Cadastro carta 2*/
     
+    getchar();
     printf("Digite o estado: \n");
-    scanf(" %c", &estado2);
+    scanf("%s", estado2);
     
+    getchar();
     printf("Digite o codigo: \n");
     scanf("%s", codigo2);
     
@@ -55,25 +65,39 @@ int main(){
     printf("Digite o numero de pontos turisticos: \n");
     scanf("%d", &numeroPontosTuristicos2);
 
-    printf("\n ------------------------------------------------- \n");
+    system("clear");
+    printf("============Cartas Cadastradas============\n"); /*Resumo de cartas cadastradas*/
 
-    printf("\n--- Carta 01 ---\n");
-    printf("Estado: %c\n", estado);
-    printf("Código: %s\n", codigo);
+    printf("\n=== Carta %s ===\n", codigo);
+    printf("Estado: %s\n", estado);
     printf("Nome da cidade: %s\n", cidade);
     printf("População: %d\n", populacao);
     printf("Área: %.2f\n", area);
     printf("PIB: %.2f\n", pib);
     printf("Nº pontos turisticos: %d\n", numeroPontosTuristicos);
 
-    printf("\n--- Carta 02 ---\n");
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
+    printf("\n=== Carta %s ===\n", codigo2);
+    printf("Estado: %s\n", estado2);
     printf("Nome da cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f\n", area2);
     printf("PIB: %.2f\n", pib2);
-    printf("Nº pontos turisticos: %d\n", numeroPontosTuristicos2);
+    printf("Nº pontos turisticos: %d\n\n", numeroPontosTuristicos2);
 
+    printf("===========================================\n\n");
+
+    /*Comparação de cartas cadastradas*/
+
+    printf("Comparação de cartas (Atributo: População)\n");
+    printf("Carta %s - %s (%s): %d\n", codigo, cidade, estado, populacao);
+    printf("Carta %s - %s (%s): %d\n\n", codigo2, cidade2, estado2, populacao2);
+
+    if (populacao>populacao2)
+    {
+        printf("Resultado: Carta %s (%s) venceu!\n\n", codigo, cidade);
+    } else  {
+        printf("Resultado: Carta %s (%s) venceu!\n\n", codigo2, cidade2);
+    }
+    
     return 0;
 }
